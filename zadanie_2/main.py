6e4aa4868845a250cdc46ba56ee75f599ac962f3
Przemysław Lis <229940@edu.p.lodz.pl>
2021-03-23 11:44:08 +0100

    setup task

--- zadanie_2/main.py
+++ zadanie_2/main.py
@@ -1,1 +1,31 @@
 print("----Zadanie 2 ----")
+
+#####################
+# CONSTANT
+#####################
+
+macierz_1 = [
+    [3, 3, 1],
+    [2, 5, 7],
+    [1, 2, 1],
+]
+macierz_1_wynik = [
+    12,
+    33,
+    8,
+]
+
+
+#####################
+# FUNCTIONS
+#####################
+
+def metoda_jordana():
+    # przekształcenie na macierz_1 jednostkową i wtedy macierz_1_wynik przekształcu się na wynik xD
+    pass
+
+
+#####################
+# MAIN
+#####################
+pass
