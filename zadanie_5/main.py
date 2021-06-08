1deaddd032f90f29e502488177e1b1dce8241018
Wojciech Majchrzak <w.majchrzak00@gmail.com>
2021-06-08 15:46:08 +0200

    poprawione

--- zadanie_5/main.py
+++ zadanie_5/main.py
@@ -7,7 +7,6 @@ from aproksymacje import blad_aproksymacji, lista_wspolczynnikow, wartosc_wielom
 import wielomian
 
 tab_wsp = []
-zmienna_iteracyjna = 1
 stopien_apro = 0
 
 questions = [
@@ -21,9 +20,6 @@ questions = [
     inquirer.List(name='kryterium',
                   message="Dokonaj wyboru kryterium aproksymacji: ",
                   choices=["Kryterium dokladnosci", "Kryterium stopnia wielomianu aproksymujacego"]),
-
-    inquirer.Text(name='stopien',
-                  message="Podaj stopien wielomianu aproksymacyjnego: "),
 ]
 answers = inquirer.prompt(questions)
 
@@ -32,19 +28,25 @@ questions2 = [
                   message="Podaj oczekiwany maksymaly blad aproksymacji: "),
 ]
 
+questions3 = [
+    inquirer.Text(name='stopien',
+                  message="Podaj stopien wielomianu aproksymacyjnego: "),
+]
+
 if answers["kryterium"] == "Kryterium dokladnosci":
     answers2 = inquirer.prompt(questions2)
     stopien_apro = 1
     dokladnosc = True
     while dokladnosc:
         tab_wsp = lista_wspolczynnikow(answers["wybor_funkcji"], int(answers["wezly"]), stopien_apro)
-        if blad_aproksymacji(answers["wybor_funkcji"], stopien_apro, tab_wsp, int(answers["wezly"])) < int(answers2["blad"]):
+        if blad_aproksymacji(answers["wybor_funkcji"], stopien_apro, tab_wsp, int(answers["wezly"])) < float(answers2["blad"]):
             dokladnosc = False
         else:
             stopien_apro += 1
 
 if answers["kryterium"] == "Kryterium stopnia wielomianu aproksymujacego":
-    stopien_apro = int(answers["stopien"])
+    answers3 = inquirer.prompt(questions3)
+    stopien_apro = int(answers3["stopien"])
     tab_wsp = lista_wspolczynnikow(answers["wybor_funkcji"], int(answers["wezly"]), stopien_apro)
 
 arg = np.linspace(-1, 1, 1000)
@@ -52,12 +54,15 @@ pb.plot(arg, wartosc_funkcji(arg, answers["wybor_funkcji"]), label='funkcja apro
 pb.plot(arg, wartosc_wielomianu(stopien_apro, arg, tab_wsp), label='aproksymacja', linestyle=':')
 pb.xlabel("x")  # opis osi x
 pb.ylabel("y")  # opis osi y
-
 fig = pb.gcf()
-fig.canvas.set_window_title('Wykres ' + str(zmienna_iteracyjna))
-
+fig.canvas.set_window_title('Wykres')
 pb.grid(True)
 pb.legend(loc='upper right')  # tworzy legendę wykresu
+wzor_apro = wielomian.Polynomial(tab_wsp[::-1])
+print("""Wzor wielomianu aproksymacyjnego: 
+{}
+""".format(wzor_apro))
+print("Blad aproksymacji: {}".format(blad_aproksymacji(answers["wybor_funkcji"], stopien_apro, tab_wsp, int(answers["wezly"]))))
 pb.title("f(x) = " + str(wzor_funkcji(answers["wybor_funkcji"])))
 pb.show()
 
